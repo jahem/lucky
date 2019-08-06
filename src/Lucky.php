394466ea3576cbb9a978f3ad88707ef327b3b7dc
jahem <13680653051@163.com>
2019-08-06 10:36:27 +0800

    lucky

---
+++ src/Lucky.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @author jahem<1094080047@qq.com>
+ */
+
+namespace Lucky;
+
+class Lucky {
+
+    /**
+     * 抽奖数据
+     * @var array 
+     */
+    protected $data;
+
+    /**
+     * 总计
+     * @var int 
+     */
+    protected $sum;
+
+    public function __construct(array $data) {
+        $this->data = $data; //抽奖数据
+        $this->sum = 0; //总计
+    }
+
+    /**
+     * 初始化
+     */
+    public function init() {
+        //格式化数据
+        foreach ($this->data as $value) {
+            $this->data[$value['id']] = $value;
+            $this->sum = $this->sum + $value;
+        }
+    }
+
+    /**
+     * 开始
+     */
+    public function start() {
+        //中奖项
+        $id = '';
+        //概率数组循环
+        foreach ($this->arr as $k => $v) {
+            $randNum = mt_rand(1, $this->sum);
+            if ($randNum <= $v) {
+                $id = $k;
+                break;
+            } else {
+                $this->sum -= $v;
+            }
+        }
+        //中奖项
+        return $id;
+    }
+
+}
